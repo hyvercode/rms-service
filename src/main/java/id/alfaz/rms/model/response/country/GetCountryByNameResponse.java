1655ab23d7d637b8e0df8e17e756f93dd5f060b5
cigist <cigist.developer@gmail.com>
2020-06-22 22:05:17 +0700

    Add list county by name

--- src/main/java/id/alfaz/rms/model/response/country/GetCountryByNameResponse.java
+++ src/main/java/id/alfaz/rms/model/response/country/GetCountryByNameResponse.java
@@ -12,5 +12,6 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class GetCountryByNameResponse extends BaseResponse {
     private String countryId;
+    private String countryCode;
     private String countryName;
 }
